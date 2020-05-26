49271a3fec5731a500352001f8b42ddc5984752c
Jamal Nassar <jamaleddinenassar@gmail.com>
2020-05-27 01:18:35 +0400

    branch06

--- ppmtool/src/main/java/com/nano/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/com/nano/ppmtool/domain/Project.java
@@ -1,5 +1,120 @@
 package com.nano.ppmtool.domain;
 
-public class Project {
+import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+import javax.persistence.*;
+
+@Entity
+public class Project {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Project name is required")
+	private String projectName;
+	@NotBlank(message = "Project identifier is required")
+	@Size(min=4, max=5, message="Please user 4 or 5 characters")
+	@Column(updatable = false)
+	private String projectIdentifier;
+	@NotBlank(message = "Project description is required")
+	private String description;
+	@JsonFormat(pattern="yyyy-mm-dd")
+	private Date startDate;
+	@JsonFormat(pattern="yyyy-mm-dd")
+	private Date endDate;
+	@JsonFormat(pattern="yyyy-mm-dd")
+	private Date create_At;
+	@JsonFormat(pattern="yyyy-mm-dd")
+	private Date update_At;
+	
+	public Project() {
+		
+	}
+	
+	@PrePersist
+	protected void onCreate() {
+		this.create_At = new Date();
+	}
+	
+	@PreUpdate
+	protected void onUpdate() {
+		this.update_At = new Date();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectIdentifier() {
+		return projectIdentifier;
+	}
+
+	public void setProjectIdentifier(String projectIdentifier) {
+		this.projectIdentifier = projectIdentifier;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public Date getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+
+	public Date getCreate_At() {
+		return create_At;
+	}
+
+	public void setCreate_At(Date create_At) {
+		this.create_At = create_At;
+	}
+
+	public Date getUpdate_At() {
+		return update_At;
+	}
+
+	public void setUpdate_At(Date update_At) {
+		this.update_At = update_At;
+	}
+	
+	
 }

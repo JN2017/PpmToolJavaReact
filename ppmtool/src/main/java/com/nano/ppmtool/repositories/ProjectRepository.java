d1de1ad10ef175cec3024d3a089e64be7d8feacf
Jamal Nassar <jamaleddinenassar@gmail.com>
2020-05-28 18:56:55 +0400

    server ready + initial app react

--- ppmtool/src/main/java/com/nano/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/com/nano/ppmtool/repositories/ProjectRepository.java
@@ -11,4 +11,11 @@ public interface ProjectRepository extends CrudRepository<Project, Long>{
 	@Override
 	Iterable<Project> findAllById(Iterable<Long> iterable);
 	
+	 Project findByProjectIdentifier(String projectId);
+
+	@Override
+	Iterable<Project> findAll();
+	 
+	 
+	
 }
--- ppmtool/src/main/java/com/nano/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/nano/ppmtool/services/ProjectService.java
@@ -21,8 +21,27 @@ public class ProjectService {
 			return projectRepository.save(project);
 		}catch(Exception e) {
 			throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists!!");
-		}
-			
+		}	
+	}
+	
+	public Project findProjectByIdentifier(String projectId) {
 		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException("Project ID '" + projectId +"' do not exist!");
+		}
+		return project;
+	}
+	
+	public Iterable<Project> findAllprojects(){
+		return projectRepository.findAll();
+	}
+	
+	public void deleteProjectByIdentifier(String projectId) {
+		Project project= projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if (project == null) {
+			throw new ProjectIdException("Cannot delete project with ID: '" + projectId +"' , this project doesn't exist!");
+		}
+		projectRepository.delete(project);
 	}
 }

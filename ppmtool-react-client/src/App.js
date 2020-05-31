65028a70d15ef2fdb5645c211a17572df21bd96e
Jamal Nassar <jamaleddinenassar@gmail.com>
2020-05-31 10:56:02 +0400

    redux arch & project UI list

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,15 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Dashboard />
-        <Dashboard />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard}></Route>
+            <Route exact path="/addProject" component={AddProject}></Route>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
